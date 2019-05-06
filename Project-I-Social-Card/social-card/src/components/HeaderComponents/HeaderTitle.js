f01ece9357bc4adcb914e658ec6b2b84eeab32dd
sesinos tesfamariam <sesinostesfamariam@sesinoss-mbp.attlocal.net>
2019-05-06 16:24:20 -0700

    sesinos-add-before-end-of-day

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,2 +1,18 @@
 import React from 'react';
 import './Header.css';
+import HeaderContent from './HeaderContent';
+
+const HeaderTitle = () => {
+    return (
+      <div >
+        <div>
+          <h1>Lambda School</h1>
+          <span className="time-stamp">('01/27/2999')</span>
+          <HeaderContent />
+        </div>
+      </div>
+    );
+  };
+  
+  export default HeaderTitle;
+  
