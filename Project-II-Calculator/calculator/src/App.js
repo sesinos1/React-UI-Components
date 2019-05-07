ecd76f75d04fb2035890d4f2be57a8a2bf9bfc3b
sesinos tesfamariam <sesinostesfamariam@sesinoss-mbp.attlocal.net>
2019-05-07 13:36:33 -0700

    git-add-wasnt-working-so-all-in-one-commit

--- Project-II-Calculator/calculator/src/App.js
+++ Project-II-Calculator/calculator/src/App.js
@@ -1,23 +1,44 @@
 import React from 'react';
 import './App.css';
+import CalculatorDisplay from './components/DisplayComponents/CalculatorDisplay';
+import ButtonContainer from './components/ButtonComponents/ButtonContainer';
+import ActionButton from './components/ButtonComponents/ActionButton';
+import NumberButton from './components/ButtonComponents/NumberButton';
 
-const App = () => {
-  return (
-    <div>
-      <h3>Welcome to React Calculator</h3>
-      <p>
-        We have given you a starter project. You'll want to build out your
-        components in their respective files, remove this code and replace it
-        with the proper components.
-      </p>
-      <p>
-        <strong>
-          Don't forget to `default export` your components and import them here
-          inside of this file in order to make them work.
-        </strong>
-      </p>
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <div className="calculator-wrapper">
+        <CalculatorDisplay />
+        <ButtonContainer className="top-buttons">
+          <ActionButton className="matching-button" text="clear" />
+          <NumberButton buttonStyle="red-button" text="÷" />
+        </ButtonContainer>
+        <ButtonContainer>
+          <NumberButton buttonStyle="small-button" text="7" />
+          <NumberButton buttonStyle="small-button" text="8" />
+          <NumberButton buttonStyle="small-button" text="9" />
+          <NumberButton buttonStyle="red-button" text="X" />
+        </ButtonContainer>
+        <ButtonContainer>
+          <NumberButton buttonStyle="small-button" text="4" />
+          <NumberButton buttonStyle="small-button" text="5" />
+          <NumberButton buttonStyle="small-button" text="6" />
+          <NumberButton buttonStyle="red-button" text="-" />
+        </ButtonContainer>
+        <ButtonContainer>
+          <NumberButton buttonStyle="small-button" text="1" />
+          <NumberButton buttonStyle="small-button" text="2" />
+          <NumberButton buttonStyle="small-button" text="3" />
+          <NumberButton buttonStyle="red-button" text="+" />
+        </ButtonContainer>
+        <ButtonContainer>
+          <ActionButton className="matching-button" text="0" />
+          <NumberButton buttonStyle="red-button" text="=" />
+        </ButtonContainer>
+      </div>
+    );
+  };
 };
 
 export default App;
